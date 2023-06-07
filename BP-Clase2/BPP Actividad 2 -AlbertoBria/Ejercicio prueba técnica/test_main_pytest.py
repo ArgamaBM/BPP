3c622b150d01225adc3e19aa8ddf92c039a3d23e
AlbertoB <alberto.briamad@gmail.com>
2023-06-07 18:20:05 +0200

    cambio en nombre de archivos actividad 2

--- BP-Clase2/BPP Actividad 2 -AlbertoBria/Ejercicio prueba técnica/test_main_pytest.py
+++ BP-Clase2/BPP Actividad 2 -AlbertoBria/Ejercicio prueba técnica/test_main_pytest.py
@@ -1,107 +1,172 @@
-import unittest
-import numpy as np
+
+
 from main import *
+import pytest
+import numpy as np
 
-class Test_EMController(unittest.TestCase):
-    def setUp(self):
-        self.controller = EMController(0, 0, 0, 0, None, "", "")
 
-    def tearDown(self):
-        pass
 
-    # Test basic battery scenarios
-    def test_basic_battery_charge(self):
-        self.controller.pv_production = 6000
-        self.controller.house_consumption = 4000
-        self.controller.storage_capacity = 5000
-        self.controller.batt_state = 0
-        self.controller.key = None
-        self.controller.commands = ""
-        self.controller.result = 'Charge'
+# Main requirement: test the algorithm
 
-        self.controller.power_management()
-        self.assertEqual(self.controller.get(), self.controller.result)
 
-        self.controller.reset_state()
-        self.assertNotEqual(self.controller.get(), self.controller.result)
+# Register fixtures. Encapsulate functions
+@pytest.fixture
+def pv_production(request):
+    return request.param
 
-    def test_basic_battery_sell_to_grid(self):
-        self.controller.pv_production = 8000
-        self.controller.house_consumption = 3000
-        self.controller.storage_capacity = 5000
-        self.controller.batt_state = 1
-        self.controller.key = None
-        self.controller.commands = ""
-        self.controller.result = 'Sell to grid'
+@pytest.fixture
+def house_consumption(request):
+    return request.param
 
-        self.controller.power_management()
-        self.assertEqual(self.controller.get(), self.controller.result)
+@pytest.fixture
+def storage_capacity(request):
+    return request.param
 
-        self.controller.reset_state()
-        self.assertNotEqual(self.controller.get(), self.controller.result)
+@pytest.fixture
+def batt_state(request):
+    return request.param
 
-    def test_basic_battery_discharge(self):
-        self.controller.pv_production = 2000
-        self.controller.house_consumption = 6000
-        self.controller.storage_capacity = 5000
-        self.controller.batt_state = 1
-        self.controller.key = None
-        self.controller.commands = ""
-        self.controller.result = 'Discharge'
+@pytest.fixture
+def key(request):
+    return request.param
 
-        self.controller.power_management()
-        self.assertEqual(self.controller.get(), self.controller.result)
+@pytest.fixture
+def commands(request):
+    return request.param
 
-        self.controller.reset_state()
-        self.assertNotEqual(self.controller.get(), self.controller.result)
+@pytest.fixture
+def result(request):
+    return request.param
 
-    def test_basic_battery_buy_from_grid(self):
-        self.controller.pv_production = 1000
-        self.controller.house_consumption = 8000
-        self.controller.storage_capacity = 5000
-        self.controller.batt_state = 0
-        self.controller.key = None
-        self.controller.commands = ""
-        self.controller.result = 'Buy from grid'
 
-        self.controller.power_management()
-        self.assertEqual(self.controller.get(), self.controller.result)
+@pytest.fixture
+def controller(pv_production, house_consumption, storage_capacity, batt_state,
+                key, commands,result):
+    controller = EMController(pv_production, house_consumption, storage_capacity, batt_state,
+                               key, commands,result)
+    return controller
 
-        self.controller.reset_state()
-        self.assertNotEqual(self.controller.get(), self.controller.result)
 
-    def test_basic_battery_pv_supplies_house(self):
-        self.controller.pv_production = 5000
-        self.controller.house_consumption = 5000
-        self.controller.storage_capacity = 5000
-        self.controller.batt_state = 0
-        self.controller.key = None
-        self.controller.commands = ""
-        self.controller.result = 'PV supplies house'
 
-        self.controller.power_management()
-        self.assertEqual(self.controller.get(), self.controller.result)
+# Test Cases. We give list of tuples with different case scenarios, using "constructors" (fixtures)
+@pytest.mark.parametrize("pv_production, house_consumption, storage_capacity,\
+                          batt_state, key, commands,result",
+                         [
+# Basic Battery Scenarios
+(6000, 4000, 5000, 0, None, "",'Charge'), #Scenario 1: Basic Battery -> Charge
+(8000, 3000, 5000, 1, None, "",'Sell to grid'), #Scenario 2: Basic Battery -> Sell to grid
+(2000, 6000, 5000, 1, None, "",'Discharge'), #Scenario 3: Basic Battery -> Discharge"
+(1000, 8000, 5000, 0, None, "",'Buy from grid'), #Scenario 4: Basic Battery -> Buy from grid"
+(5000, 5000, 5000, 0, None, "",'PV supplies house'), #Scenario 5: Basic Battery -> PV supplies house"
+# Standard Battery Scenarios
+(9000, 7000, 10000, 0, None, "",'Charge'), #Scenario 5: Standard Battery -> Charge
+(10000, 5000, 10000, 1, None, "",'Sell to grid'), #Scenario 6: Standard Battery -> Sell to grid
+(4000, 9000, 10000, 1, None, "",'Discharge'), #Scenario 7: Standard Battery -> Discharge
+(1000, 17000, 10000, 0, None, "",'Buy from grid'), #Scenario 8: Standard Battery -> Buy from grid
+(5000, 5000, 5000, 0, None, "",'PV supplies house'), #Scenario 5: Standard Battery -> PV supplies house"
+# Pro Battery Scenario
+(12000, 10000, 15000, 0, None, "",'Charge'), #Scenario 9: Pro Battery -> Charge
+(13000, 9000, 15000, 1, None, "",'Sell to grid'), #Scenario 10: Pro Battery -> Sell to grid
+(4000, 12000, 15000, 1, None, "",'Discharge'), #Scenario 11: Pro Battery -> Discharge
+(2000, 17000, 15000, 0, None, "",'Buy from grid'),  #Scenario 12: Pro Battery -> Buy from grid
+(5000, 5000, 5000, 0, None, "",'PV supplies house') #Scenario 5: Pro Battery -> PV supplies house"
+                    ],
+                                                                        
+                         indirect=["pv_production", "house_consumption", "storage_capacity",
+                                    "batt_state","key", "commands"])
 
-        self.controller.reset_state()
-        self.assertNotEqual(self.controller.get(), self.controller.result)
+def test_power_management_scenario(controller):
+    try:
 
-    # Test standard battery scenarios
-    def test_standard_battery_charge(self):
-        self.controller.pv_production = 9000
-        self.controller.house_consumption = 7000
-        self.controller.storage_capacity = 10000
-        self.controller.batt_state = 0
-        self.controller.key = None
-        self.controller.commands = ""
-        self.controller.result = 'Charge'
+        controller.power_management()
+        assert controller.get() == controller.result
 
-        self.controller.power_management()
-        self.assertEqual(self.controller.get(), self.controller.result)
+        #Reset controller
+        controller.reset_state()
+        assert controller.get() != controller.result
 
-        self.controller.reset_state()
-        self.assertNotEqual(self.controller.get(), self.controller.result)
+    except Exception as e:
+       print('ERROR in test_power_management_scenairo: ',e)
 
+
+
+
+#Test case 2 (random).Requirement 2: Implement fibonacci serie for generate random numbers
+@pytest.mark.parametrize("pv_production,house_consumption,storage_capacity, batt_state, \
+                         key, commands, result",
+                         [
+        # Basic Battery Scenarios
+        (0,0,5000, 0, None, "",None),    
+        (0,0,5000, 1, None, "",None),    
+        (0,0,5000, 1, None, "",None),    
+        (0,0,5000, 0, None, "",None),    
+        # Standard Battery Scenarios
+        (0,0,10000, 0, None, "",None),   
+        (0,0,10000, 1, None, "",None),   
+        (0,0,10000, 1, None, "",None),   
+        (0,0,10000, 0, None, "",None),   
+        # Pro Battery Scenario
+        (0,0,15000, 0, None, "",None),   
+        (0,0,15000, 1, None, "",None),   
+        (0,0,15000, 1, None, "",None),   
+        (0,0,15000, 0, None, "",None)    
+                        ],
+                                                                        
+                         indirect=["pv_production","house_consumption","storage_capacity", 
+                                   "batt_state","key","commands","result"])
+
+def test_power_management_random_scenario(controller):
+    try:
+        
+        def fibonacci(n):
+          fib_sequence = np.array([0, 1])
+          for i in range(2, n):
+              fib_sequence = np.append(fib_sequence, 
+                                       fib_sequence[i-1] + fib_sequence[i-2])
+          return fib_sequence
+        
+        #Generate Fibonacci sequence up to 20 numbers
+        fib_sequence = fibonacci(10)
+        random_fib = np.random.choice(fib_sequence)*500
+        random_fib2 = np.random.choice(fib_sequence)*500
+
+        #Update values
+        controller.pv_production = random_fib
+        controller.house_consumption = random_fib2
+
+        #Run algorithm
+        controller.power_management()
+
+        '''
+        #Discoment this line for checking the random numbers.
+        print(f'Pv_production:{controller.pv_production}\
+            House_consumption:{controller.house_consumption}\
+            Set_of_batterires:{controller.storage_capacity}\
+            batt_state:{str(controller.batt_state)}\
+            Key: {controller.key}\
+            Case: {controller.result}')
+        '''
+        
+        assert controller.get() == controller.result
+        
+        #Reset controller
+        controller.reset_state()
+        assert controller.get() != controller.result
     
+    except Exception as e:
+       print('ERROR in test_power_management_random_scenairo: ',e)
 
+#Inicialize the script
 if __name__ == '__main__':
-    unittest.main()
+    '''
+    # Discoment this part of the code for debbug. All parameter can be modified
+    # for testing specific scenarios.
+
+    aux = EMController(0, 0, 10000, 1, None, "",'')
+    aux2 = EMController(0, 0, 10000, 0, None, "",'')
+    test_power_management_random_scenario(aux)
+    test_power_management_random_scenario(aux2)
+    '''
+    pytest.main(['-vv'])
+
+
